36f83a992b926917dbce2534db1c767dac8d336a
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-03-15 01:00:37 +0500

    Created Homepage design

--- src/main/java/com/weatherapp/weatherapplication/App.java
+++ src/main/java/com/weatherapp/weatherapplication/App.java
@@ -4,15 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 767, 443);
+        stage.setTitle("Weather Application");
+        scene.getStylesheets().add(getClass().getResource("/styling/style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.weatherapp.weatherapplication {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
 
     opens com.weatherapp.weatherapplication to javafx.fxml;
     exports com.weatherapp.weatherapplication;
